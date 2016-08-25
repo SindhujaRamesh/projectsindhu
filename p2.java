49669615fbcf1a174c0f7dcf545c69e64f8c06c8
SindhujaRamesh <sindhu8526@gmail.com>
2016-08-25 10:41:00 +0530

    Create p2.java

---
+++ p2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class p2
+{
+public static void main(String args[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
