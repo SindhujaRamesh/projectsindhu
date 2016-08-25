6eec4a95bfa7606d5a5496b61be44be01a1b0433
SindhujaRamesh <sindhu8526@gmail.com>
2016-08-25 11:40:26 +0530

    Create aaa.java

---
+++ aaa.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class aaa
+{
+public static void main(String args[])
+{
+char ch;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter an character : ");
+        ch= scan.next().charAt(0);
+		
+        if(ch=='a' || ch=='A' || ch=='e' || ch=='E' ||
+        ch=='i' || ch=='I' || ch=='o' || ch=='O' ||
+        ch=='u' || ch=='U')
+        {
+            System.out.print("This is a Vowel");
+        }
+        else
+        {
+            System.out.print("This is not a Vowel");
+
+}
+}
+}
